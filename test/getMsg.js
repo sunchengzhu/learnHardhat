8554c96f17180a2c6a075836909e7b4a7905e781
sunchengzhu <sunchengzhu@cryptape.com>
2022-12-29 16:12:52 +0800

    区分账户数据

--- test/getMsg.js
+++ test/getMsg.js
@@ -1,18 +1,22 @@
 const {ethers} = require("hardhat");
 
 describe("get msg", function () {
-    it("get all kinds of blockchain data", async () => {
+    it("get block msg", async () => {
         const blockNumber = await ethers.provider.getBlockNumber();
         const gasPrice = await getGasPrice(ethers.provider);
-        const signers = await ethers.getSigners();
-        const weiBalance = await ethers.provider.getBalance(signers[0].address);
-        const balance = ethers.utils.formatEther(weiBalance);
-        const map = await getTxCount(ethers.provider, blockNumber, 10)
+        const bkMap = await getTxCount(ethers.provider, blockNumber, 10)
         console.log(`latest block number: ${blockNumber}`);
         console.log(`gas price: ${gasPrice} wei`);
-        console.log(`first account balance: ${balance} eth`);
-        map.forEach(function (value, key) {
+        bkMap.forEach(function (value, key) {
             console.log(`block ${key} tx count: ${value}`)
+        });
+    }).timeout(60000)
+
+    it("get account msg", async () => {
+        const signers = await ethers.getSigners();
+        const acctMap = await getSignersBalance(signers, 20);
+        acctMap.forEach(function (value, key) {
+            console.log(`${key} balance: ${value}`)
         });
     }).timeout(60000)
 })
@@ -29,6 +33,16 @@ async function getTxCount(provider, blockNumber, blockCount) {
             "0x" + (blockNumber - i).toString(16)
         ])
         map.set((blockNumber - i), parseInt(txCount, 16));
+    }
+    return map;
+}
+
+async function getSignersBalance(signers, signerNum) {
+    const map = new Map();
+    for (let i = 0; i < signerNum; i++) {
+        let weiBalance = await ethers.provider.getBalance(signers[i].address);
+        let balance = ethers.utils.formatEther(weiBalance);
+        map.set(signers[i].address, balance);
     }
     return map;
 }
