599246a78eb52173ecdda57e3e3857b6593608f2
sunchengzhu <sunchengzhu@cryptape.com>
2023-04-25 21:14:43 +0800

    注释微调

--- hardhat.config.js
+++ hardhat.config.js
@@ -63,7 +63,7 @@ module.exports = {
         depositAmount: 0.01,
         //账户总数
         accountsNum: 10000,
-        //每间隔1000个账户取1个账户，作为第一批账户
+        //每间隔interval个账户取1个账户，作为第一批账户
         interval: 1000
     }
 };
