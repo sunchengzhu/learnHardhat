8c48bacb25a7841703ac5c35ee574fb99586f1be
sunchengzhu <sunchengzhu@cryptape.com>
2023-04-25 21:02:33 +0800

    add `check accounts balance`

--- test/getMsg.js
+++ test/getMsg.js
@@ -9,17 +9,6 @@ describe("get msg", function () {
         console.log(`gas price: ${gasPrice} wei`)
     }).timeout(30000)
 
-    it("get accounts msg", async () => {
-        const signers = await ethers.getSigners()
-        const requestFnList = signers.map((signer) => () => ethers.provider.getBalance(signer.address))
-        const reply = await concurrentRun(requestFnList, 50, "查询所有账户余额");
-        const requestFnList1 = signers.map((signer) => () => ethers.provider.getTransactionCount(signer.address))
-        const reply1 = await concurrentRun(requestFnList1, 50, "查询所有账户nonce")
-        for (let i = 0; i < signers.length; i++) {
-            console.log(`account${i + INITIALINDEX} ${signers[i].address} balance: ${ethers.utils.formatEther(reply[i])} eth,nonce: ${reply1[i]}`)
-        }
-    }).timeout(300000)
-
     it("get an account msg", async () => {
         const address = "0x78b31C9D6ACaa8AD23B8bcab5E5D5ea438E169f0"
         const balance = await ethers.provider.getBalance(address)
@@ -81,43 +70,6 @@ async function sleep(timeOut) {
     await new Promise(r => setTimeout(r, timeOut));
 }
 
-/**
- * 执行多个异步任务
- * @param {*} fnList 任务列表
- * @param {*} max 最大并发数限制
- * @param {*} taskName 任务名称
- */
-async function concurrentRun(fnList = [], max = 5, taskName = "未命名") {
-    if (!fnList.length) return;
-    // console.log(`开始执行多个异步任务，最大并发数： ${max}`);
-    const replyList = []; // 收集任务执行结果
-    // const count = fnList.length; // 总任务数量
-    // const startTime = new Date().getTime(); // 记录任务执行开始时间
-    // let current = 0;
-    // 任务执行程序
-    const schedule = async (index) => {
-        return new Promise(async (resolve) => {
-            const fn = fnList[index];
-            if (!fn) return resolve();
-            // 执行当前异步任务
-            const reply = await fn();
-            replyList[index] = reply;
-            // console.log(`${taskName} 事务进度 ${((++current / count) * 100).toFixed(2)}% `);
-            // 执行完当前任务后，继续执行任务池的剩余任务
-            await schedule(index + max);
-            resolve();
-        });
-    };
-    // 任务池执行程序
-    const scheduleList = new Array(max)
-        .fill(0)
-        .map((_, index) => schedule(index));
-    // 使用 Promise.all 批量执行
-    await Promise.all(scheduleList);
-    // const cost = (new Date().getTime() - startTime) / 1000;
-    // console.log(`执行完成，最大并发数： ${max}，耗时：${cost}s`);
-    return replyList;
-}
 
 module.exports = {
     getTxReceipt
--- test/prepare.js
+++ test/prepare.js
@@ -1,29 +1,52 @@
 const {ethers} = require("hardhat")
 const BigNumber = require('bignumber.js');
-const {COUNT, MNEMONIC, perf} = require("../hardhat.config");
+const {COUNT, MNEMONIC, perf, INITIALINDEX} = require("../hardhat.config");
 const {expect} = require("chai");
 const {getTxReceipt} = require("./getMsg");
-const ethValue = "0.01";
-const value = ethers.utils.parseUnits(ethValue, "ether").toHexString().replaceAll("0x0", "0x");
 
 
 describe("prepare", function () {
     let gasPrice, signers
     before(async function () {
+        this.timeout(60000)
         gasPrice = await getSufficientGasPrice(ethers.provider)
         signers = await ethers.getSigners()
     })
+
     it("deposit to first batch of accounts", async function () {
         const addressList = await getAddressList(perf.accountsNum, perf.interval, MNEMONIC)
-        for (let i = 0; i < addressList.length; i++) {
-            const value = ethers.utils.parseUnits((perf.depositAmount * COUNT).toString(), "ether").toHexString().replaceAll("0x0", "0x")
-            await transferWithReceipt(signers[0].address, addressList[i], gasPrice, value)
-            const balance = await ethers.provider.getBalance(addressList[i])
-            const count = await ethers.provider.getTransactionCount(addressList[i])
-            console.log(`account${i * perf.interval} ${addressList[i]} balance: ${ethers.utils.formatEther(balance)} eth,nonce: ${count}`)
+        if (addressList.length > 1) {
+            for (let i = 1; i < addressList.length; i++) {
+                const value = ethers.utils.parseUnits((perf.depositAmount * COUNT).toString(), "ether").toHexString().replaceAll("0x0", "0x")
+                await transferWithReceipt(signers[0].address, addressList[i], gasPrice, value)
+                const balance = await ethers.provider.getBalance(addressList[i])
+                const count = await ethers.provider.getTransactionCount(addressList[i])
+                console.log(`account${i * perf.interval} ${addressList[i]} balance: ${ethers.utils.formatEther(balance)} eth,nonce: ${count}`)
+            }
         }
-    }).timeout(30000)
+        const balance = await ethers.provider.getBalance(addressList[0])
+        const count = await ethers.provider.getTransactionCount(addressList[0])
+        console.log(`account0 ${addressList[0]} balance: ${ethers.utils.formatEther(balance)} eth,nonce: ${count}`)
+    }).timeout(60000)
+
+    it("check accounts balance", async () => {
+        const signers = await ethers.getSigners()
+        const requestFnList = signers.map((signer) => () => ethers.provider.getBalance(signer.address))
+        const reply = await concurrentRun(requestFnList, 50, "查询所有账户余额");
+        const requestFnList1 = signers.map((signer) => () => ethers.provider.getTransactionCount(signer.address))
+        const reply1 = await concurrentRun(requestFnList1, 50, "查询所有账户nonce")
+        for (let i = 0; i < signers.length; i++) {
+            let balance = ethers.utils.formatEther(reply[i])
+            if (balance < perf.depositAmount) {
+                console.error(`account${i + INITIALINDEX} ${signers[i].address} balance: ${balance} eth < ${perf.depositAmount} eth,nonce: ${reply1[i]}`)
+            } else {
+                console.log(`account${i + INITIALINDEX} ${signers[i].address} balance: ${balance} eth,nonce: ${reply1[i]}`)
+            }
+        }
+    }).timeout(300000)
+
     it("deposit", async function () {
+        const value = ethers.utils.parseUnits((perf.depositAmount).toString(), "ether").toHexString().replaceAll("0x0", "0x");
         const beginNonce = await ethers.provider.getTransactionCount(signers[0].address)
         for (let i = 1; i < COUNT; i++) {
             try {
@@ -34,8 +57,9 @@ describe("prepare", function () {
             }
         }
     }).timeout(600000)
+
     it("withdraw", async function () {
-        for (let i = 1; i < COUNT; i++) {
+        for (let i = 0; i < COUNT; i++) {
             let balance = await ethers.provider.getBalance(signers[i].address)
             let value = balance.sub(ethers.BigNumber.from(21000).mul(ethers.BigNumber.from(gasPrice))).toHexString().replaceAll("0x0", "0x")
             await transferWithoutNonce(signers[i].address, signers[0].address, gasPrice, value)
@@ -96,4 +120,42 @@ async function getSufficientGasPrice(provider) {
     console.log(`my gas price: ${myGasPrice} wei`)
     const hexPrice = ethers.BigNumber.from(myGasPrice.toNumber()).toHexString().replaceAll("0x0", "0x")
     return hexPrice
+}
+
+/**
+ * 执行多个异步任务
+ * @param {*} fnList 任务列表
+ * @param {*} max 最大并发数限制
+ * @param {*} taskName 任务名称
+ */
+async function concurrentRun(fnList = [], max = 5, taskName = "未命名") {
+    if (!fnList.length) return;
+    // console.log(`开始执行多个异步任务，最大并发数： ${max}`);
+    const replyList = []; // 收集任务执行结果
+    // const count = fnList.length; // 总任务数量
+    // const startTime = new Date().getTime(); // 记录任务执行开始时间
+    // let current = 0;
+    // 任务执行程序
+    const schedule = async (index) => {
+        return new Promise(async (resolve) => {
+            const fn = fnList[index];
+            if (!fn) return resolve();
+            // 执行当前异步任务
+            const reply = await fn();
+            replyList[index] = reply;
+            // console.log(`${taskName} 事务进度 ${((++current / count) * 100).toFixed(2)}% `);
+            // 执行完当前任务后，继续执行任务池的剩余任务
+            await schedule(index + max);
+            resolve();
+        });
+    };
+    // 任务池执行程序
+    const scheduleList = new Array(max)
+        .fill(0)
+        .map((_, index) => schedule(index));
+    // 使用 Promise.all 批量执行
+    await Promise.all(scheduleList);
+    // const cost = (new Date().getTime() - startTime) / 1000;
+    // console.log(`执行完成，最大并发数： ${max}，耗时：${cost}s`);
+    return replyList;
 }
