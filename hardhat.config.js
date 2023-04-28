3e18ae7d25ee54ef0c24fc19dfb2a459b475cc45
sunchengzhu <sunchengzhu@cryptape.com>
2023-04-29 00:18:09 +0800

    更新配置文件

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "RCBd9pi7A5J4YpdugIxnyvzIFliZYZH_";
-//0x7752DCD7c6ce4aED048c028021D635CBEc6C001D
-const PRIVATE_KEY = "5af7968aa9b98c864e716ec42ea37d75a7904f0a5adc040405c24562a9f186ee";
 //default mnemonic: test test test test test test test test test test test junk
 const MNEMONIC = "already luggage walk truly world rent unlock quick tube actual acoustic spin";
 const INITIALINDEX = 0;
@@ -12,24 +9,6 @@ const COUNT = 1000;
 module.exports = {
     defaultNetwork: "localhost",
     networks: {
-        localhost: {
-            url: "http://127.0.0.1:8545",
-            accounts: {
-                mnemonic: MNEMONIC,
-                path: "m/44'/60'/0'/0",
-                initialIndex: INITIALINDEX,
-                count: 20,
-                passphrase: ""
-            }
-        },
-        goerli: {
-            url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-            accounts: [PRIVATE_KEY]
-        },
-        sepolia: {
-            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            accounts: [PRIVATE_KEY]
-        },
         gw_alphanet_v1: {
             url: "https://gw-alphanet-v1.godwoken.cf/instant-finality-hack",
             accounts: {
